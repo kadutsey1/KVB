8f3b49e0b968ffe08d2d1b98ebfdc8c353853742
kadutsey1 <kadutseyq66@gmail.com>
2024-11-22 17:51:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+# DataBasePHPClass
+define('db_host','localhost');
+
+define('db_name','rogastore');
+
+define('db_user','sitnov');
+
+define('db_pass','kadutsey')
+$db = new DB_class(db_host,db_name,db_user,db_pass)
